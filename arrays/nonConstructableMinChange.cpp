8cf3a01b577bc75bec6da0391edd5b0ad12ac978
shobhan <80381156+shogunz786@users.noreply.github.com>
2021-05-19 16:28:13 -0700

    Create nonConstructableMinChange.cpp

---
+++ arrays/nonConstructableMinChange.cpp
@@ -0,0 +1,22 @@
+/*
+ Given set of coins find the minimum denomination
+ that can't be made with the coins.
+ input:"coins": [5, 7, 1, 1, 2, 3, 22]
+ output:20
+ */
+ 
+#include <vector>
+using namespace std;
+
+//O(nlogn) time and space O(1)
+int nonConstructibleChange(vector<int> coins) {
+	sort(coins.begin(),coins.end());
+	int currentMax = 0;
+	for(auto c: coins){
+		if(c>currentMax+1){
+			return currentMax+1;
+		}
+		currentMax+=c;
+	}
+	return currentMax+1;
+}
