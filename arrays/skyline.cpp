f6ebb79190ca95eb30ac5bb6bd16380e95319ce7
shobhan <80381156+shogunz786@users.noreply.github.com>
2021-05-24 11:50:21 -0700

    Update skyline.cpp

--- arrays/skyline.cpp
+++ arrays/skyline.cpp
@@ -1,3 +1,13 @@
+/*
+Input: Array of buildings
+       { (1, 11, 5), (2, 6, 7), (3, 13, 9), (12, 7, 16), (14, 3, 25),
+         (19, 18, 22), (23, 13, 29), (24, 4, 28) }
+Output: Skyline (an array of rectangular strips)
+        A strip has x coordinate of left side and height 
+        (1, 11), (3, 13), (9, 0), (12, 7), (16, 3), (19, 18),  
+        (22, 3), (25, 0)
+*/
+//Θ(nLogn) time and O(n) space
 vector<vector<int>> find_skyline(vector<vector<int>> building)
 {
 	vector<vector<int>> res;
