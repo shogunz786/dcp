b5929141d8c4c5f8e637806c78f6e14e1d99a41a
shobhan <80381156+shogunz786@users.noreply.github.com>
2021-05-30 15:11:57 -0700

    Update longestStrChain.cpp

--- dp/longestStrChain.cpp
+++ dp/longestStrChain.cpp
@@ -15,7 +15,7 @@ bool cmpString(string a, string b){
       return a.length()<b.length();
  }
 //O(n*longest string length) time and O(n) space
- int longestStrChain(vector<string>& words) {
+ int longestStrChainLength(vector<string>& words) {
      //sort strings to complete the chains of smallest strings first
      sort(words.begin(), words.end(), cmpString);
      unordered_map<string,int> dp;
@@ -33,4 +33,59 @@ bool cmpString(string a, string b){
          maxSize=max(maxSize, dp[w]);
      }
      return maxSize;
+}
+
+
+//O(n*longest string length) time and O(n) space
+vector<string> longestStringChainPath(vector<string> strings) {
+	//dp to track longest chain
+	unordered_map<string,int> dp;
+  //dp to track path to longest chain	
+	unordered_map<string,string> chain;
+	//capture results
+	vector<string> result;
+	//capture start string of longest chain
+	string longChainStr;
+	//one word is the base case of longest chain
+	int longChainLen=1;
+	
+	//sort to process smallest strings first
+	sort(strings.begin(),strings.end(),cmpString);
+	
+	for(auto str : strings){
+		//init chain for each word ending with empty string
+		chain[str]="";
+		for(int i=0; i<str.length(); i++){
+			//generate substrings
+			string subStr = str.substr(0,i)+str.substr(i+1);
+			//if no path found then set length to 1
+			if(dp.find(subStr)==dp.end()){
+				dp[str]=max(dp[str],1);
+			}else{
+				//found a path greater than current path
+				//increment the new path with 1
+				if(dp[subStr]+1>dp[str]){
+					dp[str]=dp[subStr]+1;
+				  //capture the sub string providing
+				  //the longest path
+					chain[str]=subStr;
+				}
+			}
+		}
+		//keep track of the longest path and the starting
+		//string of the longest path
+		if(longChainLen<dp[str]){
+			longChainLen=dp[str];
+			longChainStr=str;
+		}
+	}
+	
+  while(chain.find(longChainStr)!=chain.end()){
+		   //capture path if its valid
+		   if(longChainStr!="")
+	        result.push_back(longChainStr);
+		   longChainStr=chain[longChainStr];
+ }
+	
+  return result;
 }
