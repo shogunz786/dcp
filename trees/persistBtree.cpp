64382510fb93740373996169cca3e2aeb8d70993
shobhan <80381156+shogunz786@users.noreply.github.com>
2021-05-30 09:47:16 -0700

    Update persistBtree.cpp

--- trees/persistBtree.cpp
+++ trees/persistBtree.cpp
@@ -7,6 +7,61 @@
 #include "treeNode.h"
 #include "treeNodeMgr.h"
 
+//run time O(N) space O(1)
+string TreeNodeMgr::serializeIter(TreeNode *root){
+	queue<TreeNode*> q;
+	ostringstream ostr;
+	if(root==nullptr){
+		return "# ";
+	}
+	q.push(root);
+
+	while(!q.empty()){
+		TreeNode* temp = q.front();
+		q.pop();
+		if(temp == nullptr){
+			ostr<<"# ";
+		}else{
+			ostr<<temp->val<<" ";
+			q.push(temp->left);
+			q.push(temp->right);
+		}
+	}
+	return ostr.str();
+}
+
+//run time O(N) space O(1)
+TreeNode* TreeNodeMgr::deSerializeIter(string str){
+	if(str == "# "){
+		return nullptr;
+	}
+	istringstream istr(str);
+	string v;
+	istr>>v;
+	TreeNode* root = new TreeNode(stol(v));
+	queue<TreeNode*> q;
+	q.push(root);
+	while(!q.empty()){
+		TreeNode *temp = q.front();
+		q.pop();
+		if(istr){
+			istr>>v;
+			if(v!="#"){
+				temp->left=new TreeNode(stol(v));
+				q.push(temp->left);
+			}
+		}
+		if(istr){
+			istr>>v;
+			if(v!="#"){
+				temp->right=new TreeNode(stol(v));
+				q.push(temp->right);
+			}
+		}
+	}
+	return root;
+}
+
 int main(int argc, char *argv[]){
 	string str("1 2 3 # # 4 5 # # # #");
         TreeNodeMgr mgr;
