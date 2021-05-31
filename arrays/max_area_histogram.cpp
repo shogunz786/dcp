d3606c2889193d655fdafaa912297e2520c99370
shobhan <80381156+shogunz786@users.noreply.github.com>
2021-05-31 12:56:49 -0700

    Update max_area_histogram.cpp

--- arrays/max_area_histogram.cpp
+++ arrays/max_area_histogram.cpp
@@ -1,26 +1,75 @@
-int max_hist_area(vector<int> hist)
-{
-	stack<int> si;
-	int i=0, tp_index=0, max_area=0, no_of_pillars=0;
-	while(i<hist.size())
-	{       //add to stack all heights >= top of stack
-		if(si.empty() || hist[i]>=hist[si.top()]) si.push(i++);
-		else{//reached height less than top
-                   tp_index = si.top(); si.pop();//remove top and find its area
-		   //when stack empty means no useful prior values so
-		   //current index as number of pillars e.g 5,4,3,2
-		   //else find the pillars between current and
-		   //the last valid index in stack. e.g 4,5,6,3
-		   //when at 3 i=3 and 5(i=1) is at top of stack
-		   no_of_pillars = si.empty()?i:i-si.top()-1;
-	           max_area =max(max_area, hist[tp_index]*no.of_pillars);
+
+
+/*
+ Given building heights [1, 3, 3, 2, 4, 1, 5, 3, 2]
+ find max area rectangles that can be formed, a rectagle 
+ can be formed by number of buildings of greater or equal
+ contigous buildings * the height of the current building
+ 
+ result:9
+ */
+#include <vector>
+#include <stack>
+using namespace std;
+//O(n^2) time and O(1) space
+int largestRectangleUnderSkylineBF(vector<int> buildings) {
+  //for each index scan left and right to find the max number
+	//of adjacent buildings that are greater or equal to current
+	//index
+	int area=0;
+	for(int i=0; i<buildings.size(); i++){
+		//scan left
+		int l=i-1;
+		while(l>=0 && buildings[i]<=buildings[l]){
+			l--;
+		}
+		//scan right
+		int r=i+1;
+		while(r<buildings.size() && buildings[i]<=buildings[r]){
+			r++;
+		}
+		//capture max area
+		area=max(area,(r-l-1)*buildings[i]);
+	}
+  return area;
+}
+
+//O(n) time and O(n) space
+int largestRectangleUnderSkyline(vector<int> buildings) {
+	stack<int> s;
+	
+	int i=0;
+	int maxArea=0;
+	int len=0;
+	int top=0;
+	while(i<buildings.size()){
+		//push into stack if current building height
+		//in top of the stack is less that current
+		//building height being examined
+		if(s.empty() || buildings[i]>=buildings[s.top()]){
+		   s.push(i++);	
+		}else{
+			 //if current building height being examined is smaller
+			 //than top of the stack's building height
+			 //examine max area possible with current top of stack building
+			 //and remove it from stack
+			 //the length will be current  index - top of stack
+			 //if stack empty the length will be current index
+			 //which means all the previos buildings are greater than
+			 //current index
+		   top=s.top();
+			 s.pop();
+			 
+			 len=s.empty()?i:i-top;
+			 maxArea=max(maxArea,len*buildings[top]);
 		}
 	}
-	while(!si.empty())
-	{
-                   tp_index = si.top(); si.pop();//remove top and find its area
-		   no_of_pillars = si.empty()?i:i-si.top()-1;
-	           max_area =max(max_area, hist[tp_index]*no.of_pillars);
+	//process remaining elements in the stack as before
+	while(!s.empty()){
+		   top=s.top();
+			 s.pop();
+			 len=s.empty()?i:i-top;
+			 maxArea=max(maxArea,len*buildings[top]);
 	}
-	return max_area;
+	return maxArea;
 }
