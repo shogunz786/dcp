0186e90881690e5b27b7bf330eaa923545f1c9ab
shobhan <80381156+shogunz786@users.noreply.github.com>
2021-06-01 20:16:28 -0700

    Create generateDocument.cpp

---
+++ strings/generateDocument.cpp
@@ -0,0 +1,25 @@
+/*
+ Given two strings check if all chars in one string are part of the other
+ string not necessarily in the same order.
+  Is  "AlgoExpert is the Best!" made from a subset of chars from
+ "Bste!hetsi ogEAxpelrt x ",
+ result: true
+ */
+using namespace std;
+//O(n+m) time and O(c) space - c no. of unique chars
+bool generateDocument(string characters, string document) {
+	unordered_map<char,int> mp;
+	for(auto c:characters){
+		if(mp.find(c)==mp.end())
+			mp[c]=0;
+		mp[c]++;
+	}
+	for(auto c:document){
+		if(mp.find(c)==mp.end())
+			return false;
+		else if(mp[c]<=0)
+			return false;
+		mp[c]--;
+	}
+  return true;
+}
