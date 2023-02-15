0d74014cbabedfae2dbe91d2cb30c5b607110f03
shobhanc <80381156+shogunz786@users.noreply.github.com>
2023-02-14 23:43:40 -0800

    Update maxPathSum.cpp

--- trees/maxPathSum.cpp
+++ trees/maxPathSum.cpp
@@ -11,21 +11,36 @@ public:
   void insert(vector<int> values, int i = 0);
 };
 
-vector<int> maxPathSumHelper(BinaryTree *tree){
-	if(tree == NULL)
-		return {0,INT_MIN};
-	vector<int> left = maxPathSumHelper(tree->left);
-	vector<int> right = maxPathSumHelper(tree->right);
-	int maxChildSumAsBranch=max(left[0],right[0]);
-	int maxSumAsBranch=max(maxChildSumAsBranch+tree->value, tree->value);
-	int maxSumAsRootNode = max(left[0]+tree->value+right[0],maxSumAsBranch);
-	int maxPathSum=max(left[1],max(right[1],maxSumAsRootNode));
-	return {maxSumAsBranch, maxPathSum};
+//O(n) time and O(log(n)) space
+vector<int> findMaxPath(BinaryTree *tree){
+  if(tree==NULL){
+    return vector<int>{0, INT_MIN};
+  }
+
+  vector<int> leftMax = findMaxPath(tree->left);
+  vector<int> rightMax = findMaxPath(tree->right);
+
+  int leftMaxPathSum = leftMax[1];
+  int rightMaxPathSum = rightMax[1];
+  int leftMaxBranchSum = leftMax[0];
+  int rightMaxBranchSum = rightMax[0];
+
+  //max of left or right branch
+  int maxBranchSum = max(leftMaxBranchSum, rightMaxBranchSum);
+  
+  //max of left or right branch plus current node or current node itself
+  int maxBranchSumWithRoot = max(maxBranchSum+tree->value, tree->value);
+  
+  //max of possible path with left + righ paths plus current node and max branch with root
+  int currentMaxPathSum = max(leftMaxBranchSum+tree->value+rightMaxBranchSum, maxBranchSumWithRoot);
+
+  //max of left max path or right max path or current max path
+  int maxPathSum = max(max(leftMaxPathSum, rightMaxPathSum), currentMaxPathSum);
+  
+  return vector<int>{maxBranchSumWithRoot, maxPathSum};
 }
 
-//O(n) time and O(logn) space
 int maxPathSum(BinaryTree tree) {
-  // Write your code here.
-	vector<int> ret = maxPathSumHelper(&tree);
-  return ret[1];
+  vector<int> res = findMaxPath(&tree);
+  return res[1];
 }
