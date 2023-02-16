5724606e10cf7db8e67199d153bbbbd2b8566c95
shobhanc <80381156+shogunz786@users.noreply.github.com>
2023-02-16 00:43:30 -0800

    Update underscorifySubstring.cpp

--- strings/underscorifySubstring.cpp
+++ strings/underscorifySubstring.cpp
@@ -1,71 +1,63 @@
-#include<vector>
 using namespace std;
 
-void getLocations(vector<vector<int>> &vv,string &str, string &subStr)
-{
-	int b=0;
-	while(b<str.length()){
-		if(str.length()-b < subStr.length()){
-			break;
-		}
-		int f = str.find(subStr, b);
-		if(f != string::npos){
-		   vv.push_back({f, f+static_cast<int>(subStr.length())-1});
-		   b=f+1;
-		}else{
-			break;
-		}
-	}
+//merge intervals
+//Average time O(n+m) and space O(n)
+
+//input str : testhello world but testtest is repeating testest
+//      substr: test
+//output : _test_hello world but _testtest_ is repeating _testest_
+vector<vector<int>> prepareIntervals(string &str, string &subStr){
+  int idx=0;
+  int end = 0;
+  vector<vector<int>> res;
+  while(idx<str.length()){
+    idx = str.find(subStr, idx);
+    //if no substr break
+    if(idx == string::npos){
+      break;
+    }
+
+    end = idx+static_cast<int>(subStr.length());
+    //update the end of the substr if overlap or adjacent
+    if(res.size() >0 && res.back()[1]>=idx){
+      //merge with previous
+      res.back()[1]=end;
+    }else{
+      //create new entry when no overlap
+      res.push_back(vector<int>{idx, end});
+    }
+    //try from next idx
+    idx++;
+  }
+  return res;
 }
 
-void collapse(vector<vector<int>> &vv, vector<vector<int>> &ret){
-	if(vv.size()==0){
-		return;
-	}
-	for(int i=1; i<vv.size(); i++){
-		if(vv[i][0]<=vv[i-1][1] || vv[i][0]-1==vv[i-1][1]){
-			vv[i][0]=vv[i-1][0];
-		}else{
-			ret.push_back(vv[i-1]);
-		}
-	}
-	ret.push_back(vv[vv.size()-1]);
-}
-
-void buildString(string &str, vector<vector<int>> &ret){
-	string res;
-	int begin=0, end=0;
-	int b=0;
-	for(int i=0; i<ret.size(); i++){
-		if(b!=ret[i][0]){
-			res+=str.substr(b,ret[i][0]-b);
-		}
-		res+="_"+str.substr(ret[i][0],ret[i][1]-ret[i][0]+1)+"_";
-		b=ret[i][1]+1;
-	}
-	if(b<str.length()){
-			res+=str.substr(b,str.length()-b);
-	}
-	str=res;
-}
-
-//O(n+m) time and O(n) space
 string underscorifySubstring(string str, string subStr) {
-	if(str.length()<=subStr.length()){
-		return str;
-	}
-  // Write your code here.
-	vector<vector<int>> vv, ret;
-	getLocations(vv, str, subStr);
-	cout<<"Locations:"<<endl;
-	for(auto a:vv){
-		cout<<a[0]<<","<<a[1]<<endl;
-	}
-	collapse(vv, ret);
-	cout<<"Collapse:"<<endl;
-	for(auto a:ret){
-		cout<<a[0]<<","<<a[1]<<endl;
-	}
-	buildString(str, ret);
-  return str;
+  int idx = 0;
+  int len = 0;
+  vector<string> strs;
+  vector<vector<int>> intervals = prepareIntervals(str, subStr);
+  //iterate till end of the vector
+  for(auto v: intervals){
+    //first add prefix when no immediate match
+    if(v[0]!=idx){
+      //len of prefix
+      len = v[0]-idx;
+      strs.push_back(str.substr(idx, len));
+    }
+    strs.push_back("_");
+    //find len of repeating substr
+    len = v[1]-v[0];
+    //append repeating substr - testtest or testest
+    strs.push_back(str.substr(v[0], len));
+    
+    strs.push_back("_");
+    //keep track of previous end
+    idx=v[1];
+  }
+  //add remaining part of the string
+  if(idx<str.length()){
+    strs.push_back(str.substr(idx));
+  }
+  return accumulate(strs.begin(), strs.end(), string());
 }
