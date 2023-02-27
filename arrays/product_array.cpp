36a9a260ec6f283ff9684d520da01851c8e22772
shobhanc <80381156+shogunz786@users.noreply.github.com>
2023-02-27 14:15:36 -0800

    Update product_array.cpp

--- arrays/product_array.cpp
+++ arrays/product_array.cpp
@@ -4,10 +4,12 @@ vector<int> productArray2(vector<int> arr)
 	vector<int> prod(arr.length(),1);
 	for(int i=0; i<arr.size(); i++)
 	{ //compute 0 to i-1 product in product[i]
-		prod[i]=left; left*=arr[i];
+		prod[i]=left;
+		left*=arr[i];
 	}
 	for(int i=arr.size()-1; i>=0; i++)
 	{ //compute n-1 to i+1 product in product[i] 
-		prod[i]=right; right*=arr[i];
+		prod[i]*=right;
+		right*=arr[i];
 	}
 }
