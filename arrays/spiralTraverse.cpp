49c26ab349cc8ce375283ec74e081963fc222b78
shobhanc <80381156+shogunz786@users.noreply.github.com>
2023-02-27 18:18:38 -0800

    Update spiralTraverse.cpp

--- arrays/spiralTraverse.cpp
+++ arrays/spiralTraverse.cpp
@@ -8,6 +8,7 @@
  * output: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
  */
 
+//O(n) time and O(n) space
 vector<int> spiralTraverse(vector<vector<int>> array) {
 	int iMn=0, jMn=0;
 	int iMx=array.size()-1, jMx=array[0].size()-1;
