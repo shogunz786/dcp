9dec8bdfd6c2cfe4f174539f614fddf7feec6434
shobhanc <80381156+shogunz786@users.noreply.github.com>
2023-02-28 21:59:24 -0800

    Update sortedSquaredArray.py

--- arrays/sortedSquaredArray.py
+++ arrays/sortedSquaredArray.py
@@ -1,15 +1,26 @@
-//O(n) time and O(n) space
+#O(nlg(n)) time and O(n) space
+def sortedSquaredArray(array):
+    res = [0 for _ in array]
+    i = 0
+    for i in range(len(array)):
+        res[i]=array[i]*array[i]
+        i+=1
+    res.sort()
+    return res
+
+#O(n) time and O(n) space
 def sortedSquaredArray(array):
     l=0
     r=len(array)-1
-    res = list()
-    while l<=r:
-        p_l = array[l]*array[l]
-        p_r = array[r]*array[r]
-        if p_l < p_r:
-            res.append(p_l)
+    res = [0 for _ in array]
+    
+    for idx in reversed(range(len(array))):
+        l_val = array[l]
+        r_val = array[r]
+        if abs(l_val) > abs(r_val):
+            res[idx]=l_val*l_val
             l+=1
         else:
-            res.append(p_r)
+            res[idx]=r_val*r_val
             r-=1
     return res
